932eca61d8e0c854872e6e3562196481b90e8787
Neil Dahlke <neil.dahlke@gmail.com>
2020-07-16 17:22:26 -0700

    Golinting.

--- util/util.go
+++ util/util.go
@@ -6,14 +6,16 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+// ConfigLogger will setup the logged for the library.
 func ConfigLogger() {
 	log.SetFormatter(&log.JSONFormatter{})
 	log.SetOutput(os.Stdout)
 	log.SetLevel(log.InfoLevel)
 }
 
-func FileExists(filename string) bool {
-	info, err := os.Stat(filename)
+// FileExists is a simple helper to check if a file exists at a path.
+func FileExists(path string) bool {
+	info, err := os.Stat(path)
 	if os.IsNotExist(err) {
 		return false
 	}
